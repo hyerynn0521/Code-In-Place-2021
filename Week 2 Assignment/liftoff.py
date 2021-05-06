825e0d49ba7eeb268b87acb0fc130f6a7c1a9a09
hyerynn0521 <52299631+hyerynn0521@users.noreply.github.com>
2021-05-05 21:34:38 -0700

    Create liftoff.py

---
+++ Week 2 Assignment/liftoff.py
@@ -0,0 +1,14 @@
+"""
+Prints out a spaceship launch sequence.
+"""
+
+def main():
+    pass
+    i = 10
+    while i > 0:
+        print(i)
+        i -= 1
+    print("Liftoff!")
+
+if __name__ == '__main__':
+    main()
